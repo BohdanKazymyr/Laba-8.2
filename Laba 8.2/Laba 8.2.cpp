483f0ac6556b7cba068593b1973ff42932dda310
krabster <bohdan.kzymyr.ik.2022@lpnu.ua>
2022-12-01 17:22:27 +0200

    first commit

---
+++ Laba 8.2/Laba 8.2.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#pragma warning(disable : 4996)
+using namespace std;
+int Count(char* str)
+{
+	if (strlen(str) < 3)
+		return 0;
+	int k = 0;
+	for (int i = 0; str[i + 1] != 0; i++)
+		if ('a' == str[i] && str[i + 1] != '_')
+			k++;
+	return k;
+}
+int main()
+{
+	char str[101];
+	cout << "Enter string:" << endl;
+	cin.getline(str, 100);
+	cout << "String contained " << Count(str) << " words" << endl;
+	return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Laba 8.2/Laba 8.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char str[101];
+			char str = 'absc';
+			char expected = 1;
+			char actual = 0;
+			Count(str);
+			Assert::AreEqual(expected, actual);
+		}
+	};
+}
